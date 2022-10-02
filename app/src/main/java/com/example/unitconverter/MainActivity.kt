caf75ced26258dc963789594ed5806458072f24f
Acacia Holliday <acaciaholliday@gmail.com>
2022-10-02 11:52:38 -0500

    Initial commit includes xml strings file, a simple UI layout

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.unitconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.unitconverter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+   
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+
+}
