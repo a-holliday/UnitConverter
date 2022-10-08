f85629095fa25af352ba8e6a250a2b37cec2e629
Acacia Holliday <acaciaholliday@gmail.com>
2022-10-08 13:51:20 -0500

    Set click listeners for radio buttons for material menu that change the options

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -2,19 +2,60 @@ package com.example.unitconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
 import com.example.unitconverter.databinding.ActivityMainBinding
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 
 class MainActivity : AppCompatActivity() {
-   
 
+    //initializes the binding variable when needed
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // set click listeners on the radio buttons for metric and US
+
+        binding.radioButtonMetric.setOnClickListener{ changeToMetric()
+
+        }
+
+        binding.radioButtonUS.setOnClickListener{changeToUS()
+
+        }
+
+        //set an on click listener for the calculate button
+        binding.calculateButton.setOnClickListener{
+            getMeasurement()
+        }
+
+
+
 
 
 
 
     }
+    fun changeToMetric (){
+        (binding.menuParent.editText as? MaterialAutoCompleteTextView)?.setSimpleItems(R.array.metric_units)
+    }
 
+    fun changeToUS () {
+        (binding.menuParent.editText as? MaterialAutoCompleteTextView)?.setSimpleItems(R.array.US_units)
+    }
 
+    fun getMeasurement() {
+        //get input
+        val input = binding.editTextInput.text
+
+        //if input not null get
+    }
 }
+
+
+
+
+
+
