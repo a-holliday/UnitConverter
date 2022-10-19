a611de3c40f79a66c1cf4441de9b403df56d98c3
Acacia Holliday <acaciaholliday@gmail.com>
2022-10-19 09:00:11 -0500

    added functionality working but not tested

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.unitconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import com.example.unitconverter.databinding.ActivityMainBinding
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
@@ -48,9 +49,109 @@ class MainActivity : AppCompatActivity() {
 
     fun getMeasurement() {
         //get input
-        val input = binding.editTextInput.text
+        val input = binding.editTextInput.text.toString()
+        val conversionTo = binding.menu.text.toString()
 
-        //if input not null get
+
+
+        //if input not null get calculation
+        if(!input.isEmpty() && !conversionTo.isEmpty()) {
+            //binding.conversionResult.setText(input)
+            //get name of selection
+            when (conversionTo){
+                "fluid ounces to milliliters" -> flOzToMilli(input.toDouble())
+                "cups to liters" -> cupToLiter(input.toDouble())
+                "ounces to grams"-> ozToGram(input.toDouble())
+                "pounds to kilograms" -> lbToKg(input.toDouble())
+                "Fahrenheit to Celsius" -> fToCDegree(input.toDouble())
+
+                //to US system
+                "milliliters to fluid ounces" -> milliToFlOz(input.toDouble())
+                "liters to cups" -> literToCup(input.toDouble())
+                "grams to ounces" -> gramToOz(input.toDouble())
+                "kilograms to pounds" -> kgToLb(input.toDouble())
+                "Celsius to Fahrenheit" -> cToFDegree(input.toDouble())
+
+            }
+
+        }
+        else if(input.isEmpty() && conversionTo.isEmpty()){
+            var toast = Toast.makeText(this, "Please enter the amount and select the conversion type.", Toast.LENGTH_LONG)
+            toast.show()
+        }
+        else if (input.isEmpty()){
+            var toast = Toast.makeText(this, "Please enter the amount.", Toast.LENGTH_LONG)
+            toast.show()
+        }
+
+        else{
+            var toast = Toast.makeText(this, "Please select the conversion type.", Toast.LENGTH_LONG)
+            toast.show()
+        }
+
+
+    }
+
+    fun flOzToMilli(input : Double) {
+        val result = input * 29.5735296
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
+    }
+
+    fun cupToLiter(input: Double){
+        val result = input *  0.23658824
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
+
+    }
+
+    fun ozToGram(input: Double){
+        val result = input *  28.3495231
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
+    }
+
+    fun lbToKg(input: Double){
+        val result = input *  0.45359237
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
+    }
+
+    fun fToCDegree(input: Double){
+        val result = (input - 32) * (5/9)
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
+    }
+
+    fun milliToFlOz(input: Double){
+        val result = input *  0.03381402
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
+    }
+
+    fun literToCup(input: Double){
+        val result = input *  4.22675284
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
+    }
+
+    fun gramToOz(input: Double){
+        val result = input *  0.03527396
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
+    }
+
+    fun kgToLb(input: Double){
+        val result = input * 2.20462262
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
+
+    }
+
+    fun cToFDegree(input: Double){
+        val result = (input + 32) * (9/5)
+        val formattedResult = String.format("%.2f",result)
+        binding.conversionResult.setText(formattedResult)
     }
 }
 
